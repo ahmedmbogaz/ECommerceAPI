ca4540890b5a1add6f2c10270b2cf269f900448d
Ahmed Muhammed BOĞAZ <ahmedmbogaz@gmail.com>
2022-08-16 18:14:09 +0300

    a few later..

---
+++ Core/ECommerceAPI.Application/Repositories/IReadRepository.cs
@@ -0,0 +1,19 @@
+﻿using ECommerceAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Application.Repositories
+{
+    //veri tabanından veri elde etmek için bu kullanılır. IQueryable= sorgu yapmak için direk veritabanında query e ekler.
+    public interface IReadRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> method);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> method);//şarta uygun olan ilkini getir
+        Task<T> GetByIdAsync(string id);// id uygun olanı getirecek
+    }
+}
